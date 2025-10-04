f783c58fde0e099148f19a51fa7f10be7f6e13e6
canxi4589 <caotri1203@gmail.com>
2025-10-04 14:58:46 +0700

    fix

--- RiceProduction.Infrastructure/Repository/GenericRepository.cs
+++ RiceProduction.Infrastructure/Repository/GenericRepository.cs
@@ -1,12 +1,145 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Query;
+using RiceProduction.Application.Common.Interfaces;
+using RiceProduction.Application.Common.Models;
+using RiceProduction.Infrastructure.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using RiceProduction.Domain.Common;
 
 namespace RiceProduction.Infrastructure.Repository
 {
-    internal class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T>
+   where T : BaseAuditableEntity
     {
+        private readonly ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _context.Set<T>().AddRangeAsync(entities);
+        }
+
+        public void UpdateRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().UpdateRange(entities);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public async Task<bool> DeleteRangeAsync(Expression<Func<T, bool>> predicate)
+        {
+            var entities = await _context.Set<T>().Where(predicate).ToListAsync();
+            if (!entities.Any())
+                return false;
+
+            _context.Set<T>().RemoveRange(entities);
+            return true;
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.Set<T>().CountAsync();
+        }
+
+        public T? Delete(T entityToDelete)
+        {
+            if (_context.Set<T>().Entry(entityToDelete).State == EntityState.Detached)
+            {
+                _context.Set<T>().Attach(entityToDelete);
+            }
+            var deletedEntity = _context.Set<T>().Remove(entityToDelete).Entity;
+            return deletedEntity;
+        }
+
+        public T? Delete(object id)
+        {
+            var entityToDelete = _context.Set<T>().Find(id);
+            return entityToDelete == null ? null : Delete(entityToDelete);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().AnyAsync(predicate);
+        }
+
+        public async Task<T?> FindAsync(Expression<Func<T, bool>> match)
+        {
+            return await _context.Set<T>().SingleOrDefaultAsync(match);
+        }
+
+        public async Task<T?> GetEntityByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public async Task<IReadOnlyList<T>> ListAsync(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? includeProperties = null
+        )
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                query = includeProperties(query);
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            return await query.ToListAsync();
+        }
+        public async Task<T?> FindAsync(
+            Expression<Func<T, bool>> match,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? includeProperties = null)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includeProperties != null)
+            {
+                query = includeProperties(query);
+            }
+
+            return await query.SingleOrDefaultAsync(match);
+        }
+
+
     }
 }
