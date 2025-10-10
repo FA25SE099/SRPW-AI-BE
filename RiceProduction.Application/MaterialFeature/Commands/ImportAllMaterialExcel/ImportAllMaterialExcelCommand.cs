146c6523e2c5b3ec7c31c9d83d2189c4db6e78e3
Elfledian <tohuuthinh1911@gmail.com>
2025-10-10 12:13:53 +0700

    sync file

---
+++ RiceProduction.Application/MaterialFeature/Commands/ImportAllMaterialExcel/ImportAllMaterialExcelCommand.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using RiceProduction.Application.Common.Models;
+using RiceProduction.Application.Common.Models.Response.MaterialResponses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiceProduction.Application.MaterialFeature.Commands.ImportAllMaterialExcel
+{
+    public class ImportAllMaterialExcelCommand : IRequest<Result<List<MaterialResponse>>>
+    {
+        public IFormFile ExcelFile { get; set; }
+        public DateTime ImportDate { get; set; }
+    }
+}
