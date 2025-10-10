1de9c386f63f58076ccad28f693d0433a609f743
Elfledian <tohuuthinh1911@gmail.com>
2025-10-10 21:20:18 +0700

    sync file

--- RiceProduction.Application/MaterialFeature/Commands/ImportUpdateAllMaterialExcel/ImportUpdateAllMaterialExcelCommandHandler.cs
+++ RiceProduction.Application/MaterialFeature/Commands/ImportUpdateAllMaterialExcel/ImportUpdateAllMaterialExcelCommandHandler.cs
@@ -23,13 +23,55 @@ namespace RiceProduction.Application.MaterialFeature.Commands.ImportUpdateAllMat
         {
             try
             {
-                var materialList = await _genericExcel.ExcelToListT<MaterialResponse>(request.ExcelFile);
-                if (materialList == null || !materialList.Any())
+                var materialListInput = await _genericExcel.ExcelToListT<MaterialResponse>(request.ExcelFile);
+                if (materialListInput == null || !materialListInput.Any())
                 {
                     return Result<List<MaterialResponse>>.Failure("The uploaded Excel file is empty or invalid.");
                 }
-                //var result = Result<List<MaterialResponse>>.Success(materialList, "Convert excel to list success!");
-                return Result<List<MaterialResponse>>.Success(materialList, "Convert excel to list success!");
+                foreach (var material in materialListInput)
+                {
+                    var materialInDB = _unitOfWork.Repository<Material>().FindAsync(m => material.MaterialId == m.Id);
+                    if (materialInDB == null)
+                    {
+                        return Result<List<MaterialResponse>>.Failure($"The uploaded Excel file contain material name {material.Name} with ID changed. Please download file again to get the right ID!");
+                    }
+                }
+                var materialRepo = _unitOfWork.Repository<Material>();
+                var materialPriceRepo = _unitOfWork.Repository<MaterialPrice>();
+                var materialList = new List<Material>();
+                var materialPriceUpdateList = new List<MaterialPrice>();
+                var materialPriceCreateList = new List<MaterialPrice>();
+                foreach (var material in materialListInput)
+                {
+                    var materialInDB = await materialRepo.FindAsync(m => material.MaterialId == m.Id);
+                    var materialPriceInDB = materialPriceRepo.ListAsync(p => p.MaterialId == material.MaterialId && material.IsActive && p.ValidFrom <= DateTime.UtcNow)
+                        .Result.FirstOrDefault();
+                    materialInDB.Name = material.Name;
+                    materialInDB.Type = material.Type;
+                    materialInDB.AmmountPerMaterial = material.AmmountPerMaterial;
+                    materialInDB.Unit = material.Unit;
+                    materialInDB.Description = material.Description;
+                    materialInDB.Manufacturer = material.Manufacturer;
+                    materialInDB.IsActive = material.IsActive;
+                    materialPriceInDB.ValidTo = request.ImportDate;
+                    var newMaterialPrice = new MaterialPrice
+                    {
+                        MaterialId = material.MaterialId,
+                        PricePerMaterial = material.PricePerMaterial,
+                        ValidFrom = request.ImportDate,
+                        ValidTo = null
+                    };
+                    materialList.Add(materialInDB);
+                    //materialPriceCreateList.Add(materialPriceInDB);
+                    materialPriceCreateList.Add(newMaterialPrice);
+                }
+                // If all materials are valid, proceed to update
+                materialRepo.UpdateRange(materialList);
+                await materialPriceRepo.AddRangeAsync(materialPriceCreateList);
+                //materialPriceRepo.UpdateRange(materialPriceUpdateList);
+                await _unitOfWork.CompleteAsync();
+                var result = Result<List<MaterialResponse>>.Success(materialListInput, "Convert excel to list success!");
+                return Result<List<MaterialResponse>>.Success(materialListInput, "Convert excel to list success!");
 
             }
             catch (Exception ex)
