c699a6103e523111b27b090058b19ac61019ff2a
Nguyen Duc Nguyen <nguyenndse182830@fpt.edu.vn>
2025-12-15 06:46:05 +0700

    commit

---
+++ RiceProduction.Application/SupervisorFeature/Queries/GetSupervisorByClusterId/GetSupervisorByClusterIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RiceProduction.Application.Common.Models;
+
+namespace RiceProduction.Application.SupervisorFeature.Queries.GetSupervisorByClusterId
+{
+    public class GetSupervisorByClusterIdQuery : IRequest<List<SupervisorDTO>>
+    {
+        public Guid ClusterId { get; set; }
+    }
+}
